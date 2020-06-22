ca39f371534e855099dbc137e76f9331f1b204e7
Rebeca Buarque <rebecabuarque@gmail.com>
2020-06-22 18:03:50 +0100

    First Dictioanry Exercise

---
+++ ExercicioDictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ExercicioDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            string path = @"C:\Users\gerba\Desktop\College\Rebeca Coding\Curso online - Orientado Objetos C#\12- Generics, Set e dictionary\ExercicioDictionary/text.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vect = sr.ReadLine().Split(',');
+                        string name = vect[0];
+                        int votes = int.Parse(vect[1]);
+
+                        if (dic.ContainsKey(name)){
+                            dic[name] += votes;
+                        }
+
+                        else
+                        {
+                            dic[name] = votes;
+                        }
+                    }
+
+                    foreach (var item in dic)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+
+
+               
+            } catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
